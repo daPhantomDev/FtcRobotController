7d67f71af048fc089a301a4023f1fa16792e53be
Will Loehning <willloehning@gmail.com>
2026-09-09 09:18:17 -0400

    Update FTCPractice.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTCPractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTCPractice.java
@@ -1,6 +1,18 @@
 package org.firstinspires.ftc.teamcode;
 
-public class FTCPractice
-{
-    
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class FTCPractice extends OpMode {
+
+    @Override
+    public void init() {
+        telemetry.addData("Hello", "World");
+    }
+
+    @Override
+    public void loop() {
+        
+    }
 }
