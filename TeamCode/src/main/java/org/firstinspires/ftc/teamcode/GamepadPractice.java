f47508bb7e6ff13746a8714e6575d91adfca1237
daPhantomDev <willloehning@gmail.com>
2026-09-10 16:58:05 -0400

    Create GamepadPractice.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadPractice.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class GamepadPractice extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("x", gamepad1.left_stick_x);
+        telemetry.addData("y", gamepad1.left_stick_y);
+        telemetry.addData("a Button", gamepad1.a);
+    }
+}
