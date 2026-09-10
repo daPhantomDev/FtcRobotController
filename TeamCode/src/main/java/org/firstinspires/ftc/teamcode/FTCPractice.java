e8d0e25702c52f7265f075b5f241c0bf734668fc
daPhantomDev <willloehning@gmail.com>
2026-09-10 15:42:42 -0400

    Create FTCPractice.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTCPractice.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp
+public class FTCPractice extends OpMode {
+
+    private DcMotor Moter1;
+    private Servo Servo1;
+
+    @Override
+    public void init() {
+        Moter1 = hardwareMap.get(DcMotor.class, "moter1");
+        Servo1 = hardwareMap.get(Servo.class, "servo1");
+        telemetry.addData("Hello", "Will");
+    }
+
+    @Override
+    public void loop() {
+        
+    }
+}
