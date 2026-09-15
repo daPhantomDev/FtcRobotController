3ee3b68f0f3405a24b3030d697bae37cca6b423e
Will Loehning <willloehning@gmail.com>
2026-09-15 07:20:28 -0400

    Updating where files are

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practice/FTCPractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practice/FTCPractice.java
@@ -2,13 +2,21 @@ package org.firstinspires.ftc.teamcode.Practice;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
+//Ep1
 @TeleOp
 public class FTCPractice extends OpMode {
 
+    private DcMotor Moter1;
+    private Servo Servo1;
+
     @Override
     public void init() {
-        telemetry.addData("Hello", "World");
+        Moter1 = hardwareMap.get(DcMotor.class, "moter1");
+        Servo1 = hardwareMap.get(Servo.class, "servo1");
+        telemetry.addData("Hello", "Will ss");
     }
 
     @Override
@@ -16,3 +24,6 @@ public class FTCPractice extends OpMode {
 
     }
 }
+
+
+
