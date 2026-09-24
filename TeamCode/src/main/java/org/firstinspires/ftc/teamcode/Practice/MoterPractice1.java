f12f1e5e4d34013a82a048b93ddec260e2ebbbb9
Aarna <chakrabortya30@barringtonschools.org>
2026-09-24 16:54:38 -0400

    MoterPractice dive chain in works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practice/MoterPractice1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practice/MoterPractice1.java
@@ -2,22 +2,52 @@ package org.firstinspires.ftc.teamcode.Practice;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-import org.firstinspires.ftc.teamcode.Practice.mechanism.m_MoterPractice1;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp
 public class MoterPractice1 extends OpMode {
 
-    m_MoterPractice1 m_Moter = new m_MoterPractice1();
+    DcMotor m_FL;
+    DcMotor m_FR;
+    DcMotor m_BL;
+    DcMotor m_BR;
 
     @Override
     public void init() {
-        m_Moter.init(hardwareMap);
+
+        m_FL = hardwareMap.get(DcMotor.class, "moter1");
+        m_FR = hardwareMap.get(DcMotor.class, "moter2");
+        m_BL = hardwareMap.get(DcMotor.class, "moter3");
+        m_BR = hardwareMap.get(DcMotor.class, "moter4");
     }
 
     @Override
     public void loop() {
         double yVel = -gamepad1.left_stick_y;
-        m_Moter.setMoterSpd(yVel);
+        double xVel = gamepad1.left_stick_x;
+
+        //Stick drift protection
+        if(yVel <= 0.2 && yVel >= -0.2) {
+            yVel = 0;
+        }
+        if(xVel <= 0.2 && xVel >= -0.2) {
+            xVel = 0;
+        }
+
+        //Basic Forward
+        if(yVel < 1 && yVel > -1) {
+            m_FL.setPower(yVel);
+            m_FR.setPower(yVel);
+            m_BL.setPower(yVel);
+            m_BR.setPower(yVel);
+        }
+
+        //Turning
+        if(xVel < 1 && xVel > 1) {
+            //Right
+            if(xVel > 0) {
+                m_FR.setPower(0.5);
+            }
+        }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practice/mechanism/m_MoterPractice1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practice/mechanism/m_MoterPractice1.java
@@ -1,24 +1,24 @@
-package org.firstinspires.ftc.teamcode.Practice.mechanism;
-
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-
-public class m_MoterPractice1 {
-    private DcMotor Moter1;
-
-    public void init(HardwareMap hwMap) {
-
-        Moter1 = hwMap.get(DcMotor.class, "moter1");
-        Moter1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-    }
-
-    public void setMoterSpd(double spd) {
-        if (spd > 1 || spd < -1) {
-            Moter1.setPower(spd);
-        }
-        else {
-            throw new RuntimeException("A robots speed cannot be over 1 or -1");
-        }
-
-    }
-}
+//package org.firstinspires.ftc.teamcode.Practice.mechanism;
+//
+//import com.qualcomm.robotcore.hardware.DcMotor;
+//import com.qualcomm.robotcore.hardware.HardwareMap;
+//
+//public class m_MoterPractice1 {
+//    private DcMotor Moter1;
+//
+//    public void init(HardwareMap hwMap, ) {
+//
+//        Moter1 = hwMap.get(DcMotor.class, "moter1");
+//        Moter1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//    }
+//
+//    public void setMoterSpd(double spd) {
+//        if (spd < 1 || spd > -1) {
+//            Moter1.setPower(spd);
+//        }
+//        else {
+//            throw new RuntimeException("A robots speed cannot be over 1 or -1");
+//        }
+//
+//    }
+//}
